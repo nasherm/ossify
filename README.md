5a698ad8a33e70de0d142236d01d928eaeeea6bb
Nashe Mncube <mncubenashe@gmail.com>
2021-01-19 23:38:52 +0000

    Standalone executable
    Standalone executables are built which don't
    link a standard library. Data layout passed to rustc/LLVM
    needs to reassed for accuracy against Armv8

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Introduction
-WIP - OS dev with Rust on the Raspberry Pi 2, aarch64
+WIP - OS dev with Rust on aarch64.
+
+# General Info
+- We develop for an emulator of the Cortex-A77 architecture.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+// We don't want to link the standard lib
+// but rather build a free-standing OS
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+// When we don't link with the standard
+// library we need to define a panic
+// handler.
+#[panic_handler]
+fn panic(_info : &PanicInfo) -> !{
+    loop {}
+}
+
+pub extern "C" fn _start() -> !{
+    loop {}
 }
